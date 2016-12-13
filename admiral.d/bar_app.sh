6ce04f410923047fd2e7f6437b7d2dd8f7157145
Nick <solitarycypher@gmail.com>
2016-12-12 23:04:07 -0500

    removed excess bars

--- admiral.d/bar_app.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-. ~/.config/base16-env/base16-env.sh
-. ~/.config/base16-env/base16-shell.sh
-$1
--- admiral.d/date_status.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-. lemonbar_config.sh
-
-bar_clickable "xdotool key super+ctrl+1" $(date '+%R')
