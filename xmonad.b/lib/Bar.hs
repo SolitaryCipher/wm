80645e5e1d0897c56eff5489b7d3539b1da43048
solitarycypher@gmail.com <solitarycypher@gmail.com>
2016-12-15 01:08:59 -0500

    backup xmonad in git.

---
+++ xmonad.b/lib/Bar.hs
@@ -0,0 +1,43 @@
+-----------------------------------------------------------------------------
+-- | Helper functions to format output for 'bar'
+-----------------------------------------------------------------------------
+
+module Bar ( clickable
+           , reverseColors
+           , bg
+           , fg
+           , ul
+           , ol
+           , barApp
+           ) where
+
+import System.IO (hPutStrLn) 
+
+clickable :: String -> String -> String
+clickable script text = "%{A:" ++ script ++ ":}" ++ text ++ "%{A}" 
+
+reverseColors :: String -> String
+reverseColors text = "%{R}" ++ text ++ "%{R}"
+
+bg :: String -> String -> String
+bg c s = "%{B"++c++"}"++s++"%{B-}"
+
+fg :: String -> String -> String
+fg c s = "%{F"++c++"}"++s++"%{F-}"
+
+ul :: String -> String -> String
+ul c s = "%{U"++c++"}%{+u}"++s++"%{-u}%{U-}"
+
+
+ol :: String -> String -> String
+ol c s = "%{U"++c++"}%{+o}"++s++"%{-o}%{U-}"
+
+barApp pipe app = hPutStrLn pipe $ start_app app
+
+start_app app = " if [ -n $PID ] && [[ \"$(cat /proc/$PID/cmdline)\" = *urxvt* ]]; then \
+                \    kill $PID; \
+                \ fi; \
+                \ if [[ $PREV != "++app++" ]] || [ ! -d \"/proc/$PID/\" ]; then  \
+                \   urxvt -name barapp -g 22x10 -e ~/.config/admiral.d/bar_app.sh "++app++" & PID=$!; \
+                \   PREV="++app++"; \
+                \ fi"
